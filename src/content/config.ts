c7420a5f3ce099a96e00fa32430372ec5f54f442
sugoibtc <sugoibtc@users.noreply.github.com>
2026-02-20 23:46:42 +0100

    initial build: Zitadel Logz Astro site

---
+++ src/content/config.ts
@@ -0,0 +1,13 @@
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    date: z.string(),
+    category: z.string(),
+    excerpt: z.string(),
+  }),
+});
+
+export const collections = { blog };
